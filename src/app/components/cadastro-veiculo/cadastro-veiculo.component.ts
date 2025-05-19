b85fa7c7f85268ebcf2c0079b02a9fac65527d4d
AugustoMorais222 <augustodemor04@gmail.com>
2025-05-19 20:44:44 -0300

    .

--- src/app/components/cadastro-veiculo/cadastro-veiculo.component.ts
+++ src/app/components/cadastro-veiculo/cadastro-veiculo.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-cadastro-veiculo',
-  imports: [],
-  templateUrl: './cadastro-veiculo.component.html',
+  imports: [ButtonModule],
+  template:`
+    <div class="card flex justify-center">
+      <p-button label="Check" />
+    </div>
+  `,
   styleUrl: './cadastro-veiculo.component.css'
 })
 export class CadastroVeiculoComponent {
     
 }
+  
